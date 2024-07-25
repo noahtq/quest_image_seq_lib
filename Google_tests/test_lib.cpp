c0876a08cd927ba57a875f3817c24f8b97586902
noahtq <vfx.noaht@gmail.com>
2024-07-25 18:41:23 -0500

    init commit

---
+++ Google_tests/test_lib.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Noah Turnquist on 7/22/24.
+//
+
+#include "gtest/gtest.h"
+#include <opencv2/opencv.hpp>
+#include "../library.h"
+
+
+TEST(testtest, testtesttest) {
+    cv::Mat bruh;
+    hello();
+}
---
+++ library.h
@@ -0,0 +1,8 @@
+#ifndef QUEST_IMAGE_SEQ_LIB_LIBRARY_H
+#define QUEST_IMAGE_SEQ_LIB_LIBRARY_H
+
+#include <opencv2/opencv.hpp>
+
+void hello();
+
+#endif //QUEST_IMAGE_SEQ_LIB_LIBRARY_H
